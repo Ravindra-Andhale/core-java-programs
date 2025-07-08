7bc931a00761bce27ef142b40bf7abcf5663a653
Ravindra-Andhale <raviandhale8805@gmail.com>
2025-07-08 21:42:11 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args) {
+         byte a=9;
+         System.out.println(a);
+
+         char ch='M';
+         System.out.println(ch);
+
+         boolean bol=false;
+         System.out.println(bol);
+
+         float balance=1837.87f;
+         double bal=26848488.847;
+         System.out.println(balance);
+         System.out.println(bal);
+
+         int num=39;
+         System.out.println(num);
+         long sumOfNumber=83764872;
+         System.out.println(sumOfNumber);
+         short n=456;
+         System.out.println(n);
+
+
+    }
+}
