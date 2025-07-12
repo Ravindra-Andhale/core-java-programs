dc327c4aaca4f86b3b8ad186a1cf8aa0426d64a0
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-07-12 21:41:58 +0530

    Initial commit

---
+++ Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        // int a=10;
+        // int b=20;
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("Enter number a:-");
+        // int a=sc.nextInt();
+        // System.out.println("Enter number b:-");
+        // int b=sc.nextInt();
+        // int sum=a+b;
+        // System.out.print("Sum:-"+sum); 
+       // sc.close();
+
+        float c=20;
+
+     int d=c;
+        System.out.println(d);
+
+    }
+}
