f2530ba06aed9374935427195e4a3be7919249e7
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-08-06 19:53:57 +0530

    .

--- ConditionalStatement/LargestNo.java
+++ ConditionalStatement/LargestNo.java
@@ -11,11 +11,11 @@ public class LargestNo {
 
         System.out.print("Enter third Number:-");
         int num3= sc.nextInt();
-        
-        if(num1>num2 && num1>num3){
+
+        if(num1>=num2 && num1>=num3){
             System.out.print("Largest number is:-"+num1);
         }
-        else if( num2> num1 && num2>num3){
+        else if( num2>= num1 && num2>=num3){
             System.out.print("Largest number is:-"+num2);
         }
         else{
