3621fc6f5d905f17397397079f523f8f017d43c1
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-08-15 17:22:23 +0530

    While

---
+++ Loops/WhileEg.java
@@ -0,0 +1,17 @@
+package Loops;
+import java.util.Scanner;
+
+public class WhileEg {
+    public static void main(String[] args) {
+        int count =1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:-");
+        int n=sc.nextInt();
+
+        while(count<=n){
+            System.out.println(count);
+            count++;
+        } 
+        sc.close();
+    } 
+}
---
+++ Loops/WhileLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count =0;
+        while(count<10){
+            System.out.println("hello world");
+            count++;
+        }
+        System.out.println("Print Hello World 10 times");
+    }    
+}
