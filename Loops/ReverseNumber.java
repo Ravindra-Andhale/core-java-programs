7df2bfbb8766a52b672cea330e05d104a69c9ed0
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-08-19 09:28:05 +0530

    for & while loop

---
+++ Loops/ReverseNumber.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num=23104;
+        // Using for loop to reverse the number
+       /*  for( int i=0;num>0;i++){
+            int lastDigit= num%10;
+            System.out.print(lastDigit);
+            num=num/10;
+
+        }*/
+        // Using while loop to reverse the number
+         while( num>0){
+                int digit=num%10;
+                System.out.print(digit);
+                num=num/10;
+            }
+    }
+    
+}
