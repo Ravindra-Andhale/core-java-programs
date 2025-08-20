0bff619051b5a43b58b97fc93c468c1c235de802
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-08-20 19:23:52 +0530

    eg

---
+++ Loops/DoWhileEg.java
@@ -0,0 +1,25 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class DoWhileEg {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        
+        
+
+        do{
+            System.out.print("Enter a value of a:-");
+        int a = sc.nextInt();
+            if(a%10==0){
+                continue;
+            }
+            System.out.println("Not a multiple of 10:-"+a);
+            
+        }while(true);
+        
+    }
+}
+
+
+
