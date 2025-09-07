075b648a698325a5a63c93127e239c6704d19fa9
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-09-07 18:21:29 +0530

    F&M

---
+++ FunctionsAndMethods/PrimeNumber.java
@@ -0,0 +1,26 @@
+package FunctionsAndMethods;
+import java.util.Scanner;
+public class PrimeNumber {
+    public static boolean isPrime(int num){
+        boolean isPrime=true;
+        if( num==2){
+            return true;
+        }
+        for( int i=2;i<=num-1;i++){
+            if ( num%i==0){
+                isPrime=false;
+            }
+        } 
+        return isPrime;
+    } 
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(isPrime(n));
+        sc.close();
+
+    }
+
+    
+}
