4c3fb0d8f5e3431aaf19f22190883dfaafd965c3
Ravindra-Andahle <raviandhale8805@gmail.com>
2025-09-08 16:32:22 +0530

    F&M

---
+++ FunctionsAndMethods/Average.java
@@ -0,0 +1,15 @@
+package FunctionsAndMethods;
+
+public class Average {
+
+    public static void avg( int a, int b, int c){
+         int sum= a+b+c;
+         int average= sum/3;
+         System.out.println("Averagea of number is:-"+average);
+
+    }
+    public static void main(String[] args) {
+        avg(10,20,30);
+    }
+    
+}
---
+++ FunctionsAndMethods/EvenOdd.java
@@ -0,0 +1,16 @@
+package FunctionsAndMethods;
+
+public class EvenOdd {
+    public static void evenOdd( int n){
+         if (n%2==0){
+            System.out.println(n+" is even number");
+         }
+         else {
+            System.out.println(n+ " is odd number");
+         }
+    }
+    public static void main(String[] args) {
+        evenOdd(5);
+        }
+    
+}
